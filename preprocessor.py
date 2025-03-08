0a6b89be8472c5c570a16145f793f5c7541e0f39
Prashant <shishupal78008@gmail.com>
2025-03-09 01:17:21 +0530

    eee

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern='\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{1,2}\s(?:am|pm)\s-\s'
+    pattern=r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{1,2}\s(?:am|pm)\s-\s'
     messages=re.split(pattern,data)[1:]
     dates=re.findall(pattern,data)
     df=pd.DataFrame({'user_messages':messages,'message_date':dates})
@@ -11,7 +11,7 @@ def preprocess(data):
     users=[]
     messages=[]
     for message in df['user_messages']:
-        entry=re.split('([\w\W]+?):\s',message)
+        entry=re.split(r'([\w\W]+?):\s',message)
         if entry[1:]:
           users.append(entry[1])
           messages.append(entry[2])
